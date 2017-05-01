56f4c89ddcdbdfa968cf017b1a2aa207aed56f16
albanywang2016 <albanywang2016@gmail.com>
2017-05-01 14:25:24 -0400

    first commit

---
+++ src/tech/japannews/backupdb/utils/Const.java
@@ -0,0 +1,7 @@
+package tech.japannews.backupdb.utils;
+
+public class Const {
+	public static final String DATE_PATTERN_YMD = "yyyyMMdd";
+    public static final String BACKUP_DB = "http://japannews.tech/php/dbbackup.php";
+    public static final String TRUNCATE_DB = "http://japannews.tech/php/dbdelete.php";
+}
---
+++ src/tech/japannews/backupdb/utils/Utils.java
@@ -0,0 +1,13 @@
+package tech.japannews.backupdb.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class Utils {
+	public static String formatTime(LocalDateTime now) {
+		DateTimeFormatter format = DateTimeFormatter.ofPattern(Const.DATE_PATTERN_YMD);
+		return now.format(format);
+	}
+
+}
